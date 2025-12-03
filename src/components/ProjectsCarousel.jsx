dd0d4983be99273c96dd7fc6639a80910a3d1757
Blucio Castro <133692344+BlucioCastro@users.noreply.github.com>
2025-12-03 12:54:33 -0300

    implementado titulo e numero de card no carrossel

--- src/components/ProjectsCarousel.jsx
+++ src/components/ProjectsCarousel.jsx
@@ -12,69 +12,84 @@ export default function ProjectsCarousel() {
 			id: "1",
 			name: "TMDb+",
 			image: tmdbplus,
+			number: "01",
 			description:
-				"Catálogo de filmes e séries desenvolvido em React, consumindo a API da TMDB. Inclui carrosséis dinâmicos, busca, páginas de detalhes e sistema de favoritos com persistência via LocalStorage.",
+				"Catálogo de filmes e séries desenvolvido em React...",
 		},
 		{
 			id: "2",
 			name: "Hydra Landing Page",
 			image: HydraImg,
-			description:
-				"Projeto desenvolvido em React com Vite, baseado em um design encontrado no Figma. Todo o código e implementação foram feitos por mim, seguindo o layout criado pelo designer UI.",
+			number: "02",
+			description: "Projeto desenvolvido em React com Vite...",
 		},
 		{
 			id: "3",
 			name: "Product List With Cart",
 			image: ProductLWC,
+			number: "03",
 			description:
-				"Um projeto criado a partir de um desafio do Frontend Mentor para desenvolver e consolidar meus conhecimentos adquiridos.",
+				"Um projeto criado a partir de um desafio do Frontend Mentor...",
 		},
 		{
 			id: "4",
 			name: "Advice Generator App",
 			image: AdviceGenerator,
+			number: "04",
 			description:
-				"Projeto desenvolvido para reforçar meus conhecimentos em consumo de APIs e criação de interfaces dinâmicas, utilizando como base um desafio proposto pelo Frontend Mentor.",
+				"Projeto desenvolvido para reforçar meu conhecimento...",
 		},
 	];
 
 	return (
 		<div className="w-full">
-      <h1 className="text-center text-xl mb-4">My Projects</h1>
+			<h1 className="text-xl mb-4">My Projects</h1>
+			<div className="flex items-center gap-2 mb-4">
+				<span className="text-lg font-bold text-gray-500">
+					{projects[current].number}
+				</span>
+				<h2 className="text-xl font-semibold">{projects[current].name}</h2>
+			</div>
 
-      <div className="w-full overflow-hidden flex justify-center">
-        <div
-          className="flex gap-6 transition-transform duration-700"
-          style={{ transform: `translateX(-${current * 86}%)` }}
-        >
-          {projects.map((slide) => (
-            <div
-              key={slide.id}
-              className="w-[86%] shrink-0 rounded-xl overflow-hidden"
-            >
-              <img
-                src={slide.image}
-                alt={slide.name}
-                className="w-full h-full object-cover rounded-xl"
-              />
-              <p className="text-center mt-2">{slide.name}</p>
-            </div>
-          ))}
-        </div>
-      </div>
+			<div className="flex flex-col items-center">
+				<div className="w-[50%] overflow-hidden flex justify-center items-center">
+					<div
+						className="flex gap-6 transition-transform duration-700"
+						style={{ transform: `translateX(-${current * 86}%)` }}
+					>
+						{projects.map((slide, index) => {
+							const isActive = index === current;
 
-      {/* DOTS */}
-      <div className="flex justify-center mt-4 gap-3">
-        {projects.map((_, i) => (
-          <button
-            key={i}
-            onClick={() => setCurrent(i)}
-            className={`h-3 w-3 rounded-full
-              ${i === current ? "bg-amber-400 scale-110" : "bg-amber-700/40"}
-            `}
-          />
-        ))}
-      </div>
-    </div>
+							return (
+								<div
+									key={slide.id}
+									className={`w-[86%] shrink-0 overflow-hidden transition-all duration-700 ${
+										isActive ? "opacity-100" : "opacity-40"
+									}`}
+								>
+									<img
+										src={slide.image}
+										alt={slide.name}
+										className="w-160 h-full object-fill"
+									/>
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+
+			<div className="flex justify-center mt-4 gap-2">
+				{projects.map((_, i) => (
+					<button
+						key={i}
+						onClick={() => setCurrent(i)}
+						className={`h-1 w-9 rounded-lg cursor-pointer ${
+							i === current ? "bg-orange-500 scale-110" : "bg-amber-900"
+						}`}
+					></button>
+				))}
+			</div>
+		</div>
 	);
 }
