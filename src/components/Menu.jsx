a37d32a68b46fe9aeaf668c76134cc0b5d44420b
Blucio Castro <133692344+BlucioCastro@users.noreply.github.com>
2026-01-18 16:40:37 -0300

    ajuste na fonte do menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -17,13 +17,13 @@ export default function Menu({ isOpen, onNavigate }) {
 				${isOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-2 pointer-events-none"}
 			`}
 		>
-			<button onClick={() => onNavigate("home1")}>Home</button>
-			<button onClick={() => onNavigate("about")}>About</button>
-			<button onClick={() => onNavigate("projects")}>Projects</button>
-			<button onClick={() => onNavigate("contact")}>Contact</button>
+			<button onClick={() => onNavigate("home1")} className="font-medium">Home</button>
+			<button onClick={() => onNavigate("about")} className="font-medium">About</button>
+			<button onClick={() => onNavigate("projects")} className="font-medium">Projects</button>
+			<button onClick={() => onNavigate("contact")} className="font-medium">Contact</button>
 			<button
 							onClick={() => setLang(lang === "en" ? "pt" : "en")}
-							className="text-sm  px-3 py-1 cursor-pointer"
+							className="text-sm  px-3 py-1 cursor-pointer font-bold"
 						>
 							{lang === "en" ? "PT" : "EN"}
 						</button>
